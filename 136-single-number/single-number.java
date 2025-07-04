85775a3788e5100313f1e1cb25a00fc2e00ffe4b
Shourya2025 <shouryapratapsinghthakur25@gmail.com>
2025-07-04 10:59:27 +0530

    Time: 7 ms (28.74%) | Memory: 46.5 MB (17.44%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,11 +1,19 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        
-        int single =0 ;
-        // Using XOR property -----> Read this imp point
-        for(int i = 0 ; i < nums.length ; i++){
-            single = single ^ nums[i];
+       /* int result = 0 ;
+        for(int i = 0 ; i< nums.length ; i++){
+            result = result ^ nums[i];
         }
-        return single ;
+        return result;*/
+        Arrays.sort(nums);
+        if(nums.length>1){
+            for(int i =0  ; i<nums.length -1  ;i+=2 ){
+                if(nums[i] != nums[i+1]){
+                    return nums[i];
+                }
+               
+            }
+        }
+        return nums[nums.length-1];
     }
 }
