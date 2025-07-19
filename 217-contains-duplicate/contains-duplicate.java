d56be6896aaeee21fa168557947b81d3c1382989
Shourya2025 <shouryapratapsinghthakur25@gmail.com>
2025-07-19 18:55:28 +0530

    Time: 12 ms (89.83%) | Memory: 57.7 MB (92.83%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,4 +1,5 @@
-class Solution {
+class Solution { 
+    // See yaha brute force fail hogi not saying ki galat hai par hash set yes set is a container of diffenet disctint elememts jab padhogi tab samjhana 
     public boolean containsDuplicate(int[] nums) {
       /*  for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
