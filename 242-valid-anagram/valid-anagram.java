107d168f39ff64711bc87fb9dd9fbe28a78c3beb
Shourya2025 <shouryapratapsinghthakur25@gmail.com>
2025-07-22 07:40:08 +0530

    Time: 17 ms (18.16%) | Memory: 44.8 MB (34.16%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,37 +1,39 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        //HashMap Question.
-        HashMap<Character,Integer> hm = new HashMap<>();
-        for(int i = 0 ; i <s.length() ;i++){
+        HashMap<Character , Integer> hm = new HashMap<>();
+        for(int i = 0 ; i <s.length() ; i++){
             char ch = s.charAt(i);
-            if(hm.containsKey(ch) == true){
-                hm.put(ch ,hm.get(ch)+1);
+            if(hm.containsKey(ch)==true ){
+                hm.put(ch , hm.get(ch) +1);
             }
             else {
-                hm.put(ch ,1);
-
+                hm.put(ch , 1);
             }
         }
-        for(int i = 0 ; i <t.length() ;i++){
+        for(int i = 0 ; i <t.length() ; i++){
             char ch = t.charAt(i);
-            if(hm.containsKey(ch) == true){
-               if(hm.get(ch) == 1){
-                hm.remove(ch);
-               }
-               else {
-                hm.put(ch , hm.get(ch)-1);
-               }
+            if(hm.containsKey(ch) == true ){
+                if(hm.get(ch)==1){
+                    hm.remove(ch);
+                }
+                else {
+                    hm.put(ch , hm.get(ch) -1);
+                }
             }
             else {
-              return false;
-                
+               return false;
             }
         }
-        if(hm.size()>0){
+        if(hm.size() >0){
             return false ;
         }
-        else{
-            return true;
+        else {
+            return true ;
         }
+
+        
     }
 }
+
+
+
