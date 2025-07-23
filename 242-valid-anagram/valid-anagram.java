ae38ba615ef7eb2d6a5cb51bf231ba0d87233ee1
Shourya2025 <shouryapratapsinghthakur25@gmail.com>
2025-07-23 12:58:23 +0530

    Time: 3 ms (91.56%) | Memory: 44.6 MB (42.96%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,39 +1,50 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character , Integer> hm = new HashMap<>();
-        for(int i = 0 ; i <s.length() ; i++){
+        //HashMap Question.
+        /*HashMap<Character,Integer> hm = new HashMap<>();
+        for(int i = 0 ; i <s.length() ;i++){
             char ch = s.charAt(i);
-            if(hm.containsKey(ch)==true ){
-                hm.put(ch , hm.get(ch) +1);
+            if(hm.containsKey(ch) == true){
+                hm.put(ch ,hm.get(ch)+1);
             }
             else {
-                hm.put(ch , 1);
+                hm.put(ch ,1);
+
             }
         }
-        for(int i = 0 ; i <t.length() ; i++){
+        for(int i = 0 ; i <t.length() ;i++){
             char ch = t.charAt(i);
-            if(hm.containsKey(ch) == true ){
-                if(hm.get(ch)==1){
-                    hm.remove(ch);
-                }
-                else {
-                    hm.put(ch , hm.get(ch) -1);
-                }
+            if(hm.containsKey(ch) == true){
+               if(hm.get(ch) == 1){
+                hm.remove(ch);
+               }
+               else {
+                hm.put(ch , hm.get(ch)-1);
+               }
             }
             else {
-               return false;
+              return false;
+                
             }
         }
-        if(hm.size() >0){
+        if(hm.size()>0){
             return false ;
         }
-        else {
-            return true ;
+        else{
+            return true;
         }
+        */
 
-        
+
+        // Appraoch 2 ----> faster approach
+        if(s.length() != t.length()){
+            return false ;
+
+        }
+        char a[] = s.toCharArray();
+        char b[] = t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a,b);
     }
 }
-
-
-
