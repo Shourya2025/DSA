56f458d8e3ee04da7e5c9cdd1cfe2676d49389e2
Shourya2025 <shouryapratapsinghthakur25@gmail.com>
2025-07-24 16:45:45 +0530

    Time: 15 ms (5.01%) | Memory: 54.5 MB (5.47%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,5 +1,41 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
+        // if(nums.length ==1){
+        //     return nums[0];
+        // }
+        // else if(nums[0] != nums[1]){
+        //     return nums[0];
+        // }
+        // else if(nums[nums.length-1] != nums[nums.length-2] ){
+        //     return nums[nums.length -1];
+        // }
+        // int start = 0;
+        // int end = nums.length -1;
+        // while(start <= end){
+        //     int mid = (start + end)/2;
+        //     if(nums[mid] != nums[mid-1]&& nums[mid] != nums[mid +1]){
+        //         return nums[mid];
+
+        //     }
+        //     else if(mid%2 == 1 ){
+        //         if(nums[mid-1] == nums[mid]){
+        //             start = mid +1;
+        //         }
+        //         else {
+        //             end = mid -1;
+        //         }
+        //     }
+        //     else{//even index
+        //         if(nums[mid]== nums[mid+1]){
+        //             start = mid+1;
+        //         }
+        //         else {
+        //             end = mid -1;
+        //         }
+        //     }
+        // }
+        // return -1;
+       
         HashMap<Integer,Integer> hm = new HashMap<>();
         for(int i =0 ; i <nums.length ; i++){
             if(hm.containsKey(nums[i]) == true){
