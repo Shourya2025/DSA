18dff04acc2c2eb9883093aeb3db46725ba0edcf
Shourya2025 <shouryapratapsinghthakur25@gmail.com>
2025-07-29 10:11:00 +0530

    Time: 14 ms (14.74%) | Memory: 58.6 MB (77.31%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,23 +10,24 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        
         Stack<Integer> st = new Stack<>();
-        ListNode ptr1 = head ;
+        ListNode ptr1 = head;
         while(ptr1 != null){
             st.push(ptr1.val);
             ptr1 = ptr1.next;
-            //push funtion done
-        }
 
-        ListNode ptr2 = head ;
-        while(ptr2!= null){
+        }
+         ListNode ptr2 = head;
+        while(ptr2 != null){
             int val1 = st.pop();
             int val2 = ptr2.val;
-            if(val1 != val2) return false;
-            ptr2 = ptr2.next ;
-
+            if(val1 != val2 ) return false;
+            ptr2 = ptr2.next;
         }
         return true;
+
+
+
+
     }
 }
