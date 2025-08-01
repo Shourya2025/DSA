539e8b426ffc6950083351844d1563a6679c6e10
Shourya2025 <shouryapratapsinghthakur25@gmail.com>
2025-08-01 08:55:12 +0530

    Time: 1 ms (100.00%) | Memory: 44.5 MB (52.88%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -10,38 +10,41 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode ptr1 = l1;
-        ListNode ptr2 = l2;
+        
         ListNode dummy = new ListNode(-1);
-        ListNode ans = dummy ;
+        ListNode ans = dummy;
+        ListNode ptr2 = l2;
+        ListNode ptr1 = l1;
         if(ptr1 == null){
-            return l2;
+            return ptr2;
         }
         else if(ptr2 == null){
-            return l1;
+            return ptr1;
         }
-        int carry =0 ;
+        int carry = 0 ;
         while(ptr1 != null || ptr2 !=null){
-            int val1 = ptr1!=null ? ptr1.val : 0;
-            int val2 = ptr2!=null ? ptr2.val : 0;
-            int sum = (val1+val2)+carry;
-            int d =sum%10;
-            carry =sum/10;
+            int val1 = ptr1!=null ?  ptr1.val : 0;
+            int val2 = ptr2!=null ?  ptr2.val : 0;
+            int sum = val1+ val2 + carry; 
+            int d = sum%10;
+            carry = sum/10;
             ListNode temp = new ListNode(d);
-           
-            dummy.next = temp;
-            dummy =dummy.next;
-            ptr1= ptr1!=null ? ptr1.next : null;
-            ptr2= ptr2!=null ? ptr2.next : null;
-
-        }
-        if(carry>0){
-            ListNode temp = new ListNode(carry);
             dummy.next = temp;
             dummy = dummy.next;
 
+            ptr1 = ptr1!=null ? ptr1.next : null;
+            ptr2 = ptr2!=null ? ptr2.next : null;
+
 
         }
+        if(carry>0){
+             ListNode temp = new ListNode(carry);
+             dummy.next = temp;
+        }
+        
+
         return ans.next;
+
+        
     }
 }
