df1389d3e63353be7633e3bd71618c02374e6140
Shourya2025 <shouryapratapsinghthakur25@gmail.com>
2025-08-10 09:24:38 +0530

    Time: 1 ms (64.05%) | Memory: 41 MB (28.18%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,16 +1,13 @@
 class Solution {
     public boolean isHappy(int n) {
-        HashSet<Integer> hm = new HashSet<>();
-        while(n!=1){
-            if(hm.contains(n)){
-                return false ;
-            }
-            hm.add(n);
-            n = square(n);
-        }
-        return true;
-        
-    }
+      HashSet<Integer> hm = new HashSet<>();
+      while(n!=1){
+        if(hm.contains(n)) return false ;
+        hm.add(n);
+        n = square(n);
+      }
+      return true ;  
+    } 
     int square(int n){
         int sum = 0 ;
         while(n!=0){
