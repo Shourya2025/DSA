871ebe2b059cf6848e6583966b5e111fd869a2b5
Shourya2025 <shouryapratapsinghthakur25@gmail.com>
2025-08-24 10:06:10 +0530

    Time: 33 ms (80.42%) | Memory: 57.9 MB (51.82%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,18 +1,17 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        Set st = new HashSet<>();
-        int left = 0;
-        long currSum = 0 ; long maxSum = 0;
-        for(int right = 0 ; right < nums.length ; right++){
-            while(st.contains(nums[right]) || st.size()==k){
+        Set<Integer> st = new HashSet<>();
+        int left = 0 ; 
+        long currSum = 0 ; long maxSum = 0 ;
+        for(int right = 0 ; right< nums.length ; right++){
+            while(st.contains(nums[right])){
                 currSum -= nums[left];
                 st.remove(nums[left]);
                 left++;
             }
-            currSum+= nums[right];
+            currSum += nums[right];
             st.add(nums[right]);
-            
-            if(right - left + 1 == k){
+            if(right - left +1 == k){
                 maxSum = Math.max(currSum , maxSum);
                 currSum -= nums[left];
                 st.remove(nums[left]);
@@ -22,41 +21,3 @@ class Solution {
         return maxSum ;
     }
 }
-
-
-
-
-
-// import java.util.*;
-
-// class Solution {
-//     public long maximumSubarraySum(int[] nums, int k) {
-//         Set<Integer> set = new HashSet<>();
-//         int left = 0;
-//         long currSum = 0, maxSum = 0;
-
-//         for (int right = 0; right < nums.length; right++) {
-//             // While we have duplicates, move left pointer
-//             while (set.contains(nums[right])) {
-//                 set.remove(nums[left]);
-//                 currSum -= nums[left];
-//                 left++;
-//             }
-
-//             // Add the current element
-//             set.add(nums[right]);
-//             currSum += nums[right];
-
-//             // Check if window size is exactly k
-//             if (right - left + 1 == k) {
-//                 maxSum = Math.max(maxSum, currSum);
-//                 // Slide window forward
-//                 set.remove(nums[left]);
-//                 currSum -= nums[left];
-//                 left++;
-//             }
-//         }
-
-//         return maxSum;
-//     }
-// }
