425b2c9cae223309141a03a646c92e0a58471790
Shourya2025 <shouryapratapsinghthakur25@gmail.com>
2025-09-23 14:32:06 +0530

    Time: 1 ms (76.03%) | Memory: 41.6 MB (48.13%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        String arr1[] = version1.split("\\.");
+        String arr2[] = version2.split("\\.");
+        for(int i = 0 ; i < Math.max(arr1.length , arr2.length) ; i++){
+            int x = 0 , y = 0 ;
+            if(i < arr1.length){
+                x = Integer.valueOf(arr1[i]);
+
+            }
+            if(i < arr2.length){
+                y = Integer.valueOf(arr2[i]);
+            }
+            if(x>y) return 1;
+            if(x<y) return -1;
+        }
+        return 0 ;
+    }
+}
