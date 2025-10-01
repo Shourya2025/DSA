5f66a97d06083c086c6947c9a11e62150dc0ef4c
Shourya2025 <shouryapratapsinghthakur25@gmail.com>
2025-10-01 13:10:44 +0530

    Time: 3 ms (18.06%) | Memory: 44.9 MB (61.17%) - LeetSync

---
+++ 57-insert-interval/insert-interval.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int n  = intervals.length;
+
+        int[][] interval = new int[n+1][2];
+        System.arraycopy(intervals , 0 , interval , 0 , n);
+        
+        interval[n] = newInterval;
+       Arrays.sort(interval , (a,b)-> Integer.compare(a[0] , b[0]));
+     
+       int[][] res = new int[n+1][2]; 
+       res[0] = interval[0];
+       int e = 0 ;
+       for(int i = 1 ; i<= n; i++){
+        if(res[e][1] >= interval[i][0]){
+            res[e][1] = Math.max( res[e][1] , interval[i][1]);
+        }
+        else {
+            e++;
+            res[e] = interval[i];
+        }
+       }
+       return Arrays.copyOfRange(res , 0 , e+1);
+    }
+}
+
+
+
+
+
+
+
