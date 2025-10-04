565f31aee94a3fa40b47cf0a1c4bb1e6defb8656
Shourya2025 <shouryapratapsinghthakur25@gmail.com>
2025-10-04 12:15:24 +0530

    Time: 7 ms (6.08%) | Memory: 58.4 MB (14.78%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -2,25 +2,20 @@ class Solution {
     public int maxArea(int[] height) {
         int start = 0 ;
         int end = height.length -1;
-        int maxCapp= 0;
-        while(start < end ){
-            int width = end - start ;
-            int minheight = Math.min(height[start] ,height[end]);
-            int area = minheight*width;
-            maxCapp = Math.max(area , maxCapp);
+        int max = 0;
+        while(start < end){
+            int width = Math.abs(start - end);
+            int currheight = Math.min(height[start] ,height[end]);
+            int currcap = currheight*width;
+            max = Math.max(currcap , max);
             if(height[start] < height[end]){
                 start ++;
-            }
-            else {
+            }else{
                 end--;
             }
-
         }
-        return maxCapp;
-
-
-
-
+        return max;
+       
     }
 
 }
@@ -44,3 +39,34 @@ class Solution {
 //      return maxCap;
 
 // }
+
+
+
+
+
+
+
+
+
+
+
+//  int start = 0 ;
+//         int end = height.length -1;
+//         int maxCapp= 0;
+//         while(start < end ){
+//             int width = end - start ;
+//             int minheight = Math.min(height[start] ,height[end]);
+//             int area = minheight*width;
+//             maxCapp = Math.max(area , maxCapp);
+//             if(height[start] < height[end]){
+//                 start ++;
+//             }
+//             else {
+//                 end--;
+//             }
+
+//         }
+//         return maxCapp;
+
+
+
