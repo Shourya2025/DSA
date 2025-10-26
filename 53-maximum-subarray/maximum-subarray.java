ee46dbfd55817a21be9ad19aec8a768a99a33e95
Shourya2025 <shouryapratapsinghthakur25@gmail.com>
2025-10-26 08:57:52 +0530

    Time: 1 ms (99.41%) | Memory: 57.3 MB (32.47%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,19 +1,55 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int currmax = nums[0];
+       /* int currsum = nums[0];
         int max = nums[0];
-        for(int i = 1 ; i < nums.length ; i++){
-            if(currmax + nums[i]> nums[i]){
-                currmax += nums[i];
-                
+        for(int i =1 ; i<nums.length ; i++){
+            if(currsum + nums[i] > nums[i]){
+                currsum = currsum + nums[i];
             }
             else {
-                currmax = nums[i];
+                currsum = nums[i];
             }
-            if(currmax > max){
-                max = currmax;
+            max = Math.max( max,currsum  );
+            
+        }
+        return max ;*/
+
+
+
+
+        //Old Approach 
+        // int max = nums[0];
+        // int currmax = nums[0];
+        // for(int i = 1 ; i < nums.length ; i++){
+        //     if(currmax + nums[i]> nums[i]){
+        //         currmax = currmax + nums[i];
+
+        //     }
+        //     else 
+        //     {
+        //         currmax = nums[i];
+
+        //     }
+        //     max = Math.max(currmax, max);
+        // }
+        // return max;
+
+
+
+        //Kadane's Algorthim 
+        int max = Integer.MIN_VALUE;
+        int currsum = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            currsum += nums[i];
+            max = Math.max(currsum , max);
+            if(currsum < 0){
+                currsum = 0;
             }
         }
-        return max;
+        return  max;
+
+
+
+
     }
 }
