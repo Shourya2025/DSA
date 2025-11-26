37125b93347a0f5952fb0e625cd1df30b1e18e35
Shourya2025 <shouryapratapsinghthakur25@gmail.com>
2025-11-26 18:35:39 +0530

    Time: 1 ms (17.70%) | Memory: 42.7 MB (24.91%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,5 +1,15 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        //return s.toLowerCase();
+        StringBuilder sm = new StringBuilder(s); 
+        for(int i = 0 ; i <s.length() ; i++){
+            char ch = sm.charAt(i);
+            if(ch >= 'A' && ch <= 'Z'){
+                ch = (char)(ch + ('a' - 'A'));
+                sm.setCharAt(i , ch);
+            }
+
+        }
+        return sm.toString();
     }
 }
