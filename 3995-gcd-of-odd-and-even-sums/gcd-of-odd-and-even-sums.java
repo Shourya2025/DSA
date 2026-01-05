98985db79d52ea0f414601d4289b895141b74e6c
Shourya2025 <shouryapratapsinghthakur25@gmail.com>
2026-01-05 10:55:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (74.07%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,25 +1,26 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int odd = n ;
-        int osum = 0;
-        int even= n;
-        int esum =0;
-        int st =1;
-        while(odd!= 0){
-            if(st%2!=0){
-                osum = osum+st;
-                odd--;
-            }
-            st++;
-        }
-        while(even!= 0){
-            if(st%2==0){
-                esum = esum+st;
-                even--;
-            }
-            st++;
-        }
-        return GCD(osum , esum);
+        // int odd = n ;
+        // int osum = 0;
+        // int even= n;
+        // int esum =0;
+        // int st =1;
+        // while(odd!= 0){
+        //     if(st%2!=0){
+        //         osum = osum+st;
+        //         odd--;
+        //     }
+        //     st++;
+        // }
+        // while(even!= 0){
+        //     if(st%2==0){
+        //         esum = esum+st;
+        //         even--;
+        //     }
+        //     st++;
+        // }
+        // return GCD(osum , esum);
+        return n ;
     
     }
     public int GCD(int x , int y){
