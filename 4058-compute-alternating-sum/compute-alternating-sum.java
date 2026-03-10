50a7d554f6b83b828ea7da45507a795a4a0a29ee
Shourya Pratap Singh Thakur <shouryapratapsinghthakur25@gmail.com>
2026-03-10 10:33:33 +0530

    Time: 1 ms (98.58%) | Memory: 46.4 MB (84.51%) - LeetSync

---
+++ 4058-compute-alternating-sum/compute-alternating-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int alternatingSum(int[] nums) {
+        int esum = 0;
+        int osum = 0;
+        for(int i = 0 ; i < nums.length ;i++){
+            if(i%2==0){
+                esum = esum + nums[i];
+            }
+            else {
+                osum = osum + nums[i];
+            }
+        }
+        return esum-osum;
+
+    }
+}
