0e4b678451b32231e30cb655e18bb23c53ce8e9c
Shourya Pratap Singh Thakur <shouryapratapsinghthakur25@gmail.com>
2026-03-15 16:36:16 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (88.43%) - LeetSync

---
+++ 3371-harshad-number/harshad-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int j = x;
+        int sum = 0;
+        while(x!=0){
+            int d = x%10;
+            sum += d;
+            x = x/10;
+
+        }
+        if(j%sum == 0){
+            return sum;
+        }
+        else {
+            
+        return -1;
+        }
+
+
+    }
+    
+}
