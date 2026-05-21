74321fe1c6e6c6c136ce01adacab7834246d0fcb
Shourya Pratap Singh Thakur <shouryapratapsinghthakur25@gmail.com>
2026-05-21 13:27:48 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (83.94%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,12 +1,30 @@
 class Solution {
-    static int fibo(int n){
-        if (n<2){
-             return n ;
+    static int[] dp;
+    public  int fibo(int n){
+        if(n == 1 || n == 0){
+            return n ;
         }
-        return fibo(n-1) + fibo(n-2);
+        if(dp[n] != 0){
+            return dp[n];
+        }
+        int ans = fibo(n-1) + fibo(n-2);
+        dp[n] = ans;
+        return ans;
     }
     public int fib(int n) {
+        dp = new int[n+1];
         return fibo(n);
+
+    }
+}
+
+
+
+
+
+
+
+//   The Ultimate base code - naive approach 
 
         // int a = 0 , b= 1  , c  = 0 ;
         // if (n==0){
@@ -24,5 +42,17 @@ class Solution {
         // }
         // return c ;}
 
-    }
-}
+
+
+
+//      The Rescursion approach 
+// static int fibo(int n){
+//         if (n<2){
+//              return n ;
+//         }
+//         return fibo(n-1) + fibo(n-2);
+//     }
+//     public int fib(int n) {
+//         return fibo(n);
+
+//     } 
