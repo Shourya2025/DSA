ed10aa93dddac87c11af9671a1bbb142b723ae66
Shourya Pratap Singh Thakur <shouryapratapsinghthakur25@gmail.com>
2026-07-19 18:55:37 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (39.14%) - LeetSync

---
+++ 1560-number-of-students-doing-homework-at-a-given-time/number-of-students-doing-homework-at-a-given-time.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int cnt = 0;
+        for(int i = 0 ; i < startTime.length ; i++){
+            if(queryTime >= startTime[i] && queryTime<= endTime[i] ){
+                cnt++;
+            }
+        }
+        return cnt ;
+    }
+}
